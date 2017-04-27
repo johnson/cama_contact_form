8b904e3a070a8ca19fe8556970b30938369e3744
Johnson Qu <johnson.qu@dealglobe.com>
2017-04-27 18:26:25 +0800

    fix migration file version

--- db/migrate/20160517143441_create_db_structure.rb
+++ db/migrate/20160517143441_create_db_structure.rb
@@ -1,5 +1,5 @@
 # This migration comes from cama_contact_form_engine (originally 20160517143441)
-class CreateDbStructure < ActiveRecord::Migration
+class CreateDbStructure < ActiveRecord::Migration[4.2]
   def change
     unless table_exists? 'plugins_contact_forms'
       create_table :plugins_contact_forms do |t|
